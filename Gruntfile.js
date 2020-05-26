39b7efb60f8589d24a41c236609ad16f52b7aa7d
sob05001 <solomon.berhe@gmail.com>
2020-05-26 15:02:40 +0200

    setup environment

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,12 +11,31 @@ module.exports = function(grunt) {
         src: 'src/app.js',
         dest: 'dist/app.min.js'
       }
+    },
+    minifyHtml: {
+        options: {
+            cdata: true
+        },
+        dist: {
+            files: {
+                'dist/index.html': 'src/index.html'
+            }
+        }
+    },
+    copy: {
+      main: {
+        files: [
+          {expand: true, cwd: 'src/lib', src: ['./*'], dest: 'dist/lib', filter: 'isFile'},
+        ],
+      },
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify-es');
+  grunt.loadNpmTasks('grunt-minify-html');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify', 'minifyHtml','copy']);
 };
--- src/app.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var fs = require('fs');
-var index = fs.readFileSync('index.html');
-const port = 80;
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(index);
-}).listen(port, () => console.log(`releasetrain client listening at http://localhost:${port}`));
