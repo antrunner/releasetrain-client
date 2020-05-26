dc5dfaed67ca07241c38af04d0bd256dce723321
sob05001 <solomon.berhe@gmail.com>
2020-05-26 12:11:35 +0200

    Setup environment

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/app.js',
+        dest: 'dist/app.min.js'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify-es');
+  
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+};
--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var fs = require('fs');
 var index = fs.readFileSync('index.html');
+const port = 80;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end(index);
-}).listen(80);
+}).listen(port, () => console.log(`releasetrain client listening at http://localhost:${port}`));
