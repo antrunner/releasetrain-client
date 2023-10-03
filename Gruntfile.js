49c0b80a6c181a0afcd34879da9c26fe67483d95
sberhe <solomon.berhe@gmail.com>
2023-10-03 15:24:34 -0700

    refactor html code

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,13 +18,17 @@ module.exports = function(grunt) {
             },
             dist: {
                 files: {
-                    'dist/index.html': 'src/index.html',
-                    'dist/manual/index.html': 'src/manual/index.html',
-                    'dist/ext/index.html': 'src/ext/index.html',
                     'dist/ack/index.html': 'src/ack/index.html',
-                    'dist/graph/index.html': 'src/graph/index.html',
-                    'dist/edi40-2023/index.html': 'src/edi40-2023/index.html',
                     'dist/app.css': 'src/app.css',
+                    'dist/common/github.html': 'src/common/github.html',
+                    'dist/common/navigation.html': 'src/common/navigation.html',
+                    'dist/conference/index.html': 'src/conference/index.html',
+                    'dist/developer/index.html': 'src/developer/index.html',
+                    'dist/edi40/index.html': 'src/edi40/index.html',
+                    'dist/edi40-2023/index.html': 'src/edi40-2023/index.html',
+                    'dist/graph/index.html': 'src/graph/index.html',
+                    'dist/index.html': 'src/index.html',
+                    'dist/research/index.html': 'src/research/index.html',
                     'dist/reset.css': 'src/reset.css'
                 }
             }
@@ -32,10 +36,18 @@ module.exports = function(grunt) {
         copy: {
             main: {
                 files: [
-                    { expand: true, cwd: 'src/lib', src: ['./*'], dest: 'dist/lib', filter: 'isFile' },
-                    { expand: true, cwd: 'src/img', src: ['./*'], dest: 'dist/img', filter: 'isFile' },
+                    { expand: true, cwd: 'src', src: ['./*'], dest: 'dist', filter: 'isFile' },
+                    { expand: true, cwd: 'src/ack', src: ['./*'], dest: 'dist/ack', filter: 'isFile' },
+                    { expand: true, cwd: 'src/common', src: ['./*'], dest: 'dist/common', filter: 'isFile' },
+                    { expand: true, cwd: 'src/conference', src: ['./*'], dest: 'dist/conference', filter: 'isFile' },
+                    { expand: true, cwd: 'src/developer', src: ['./*'], dest: 'dist/developer', filter: 'isFile' },
+                    { expand: true, cwd: 'src/docs/paper/edi40_2020', src: ['./*'], dest: 'dist/docs/paper/edi40_2020', filter: 'isFile' },
+                    { expand: true, cwd: 'src/edi40', src: ['./*'], dest: 'dist/edi40', filter: 'isFile' },
                     { expand: true, cwd: 'src/edi40-2023/data', src: ['./*'], dest: 'dist/edi40-2023/data', filter: 'isFile' },
-                    { expand: true, cwd: 'src/docs/paper/edi40_2020', src: ['./*'], dest: 'dist/docs/paper/edi40_2020', filter: 'isFile' }
+                    { expand: true, cwd: 'src/graph', src: ['./*'], dest: 'dist/graph', filter: 'isFile' },
+                    { expand: true, cwd: 'src/img', src: ['./*'], dest: 'dist/img', filter: 'isFile' },
+                    { expand: true, cwd: 'src/lib', src: ['./*'], dest: 'dist/lib', filter: 'isFile' },
+                    { expand: true, cwd: 'src/research', src: ['./*'], dest: 'dist/research', filter: 'isFile' }
                 ],
             },
         }
