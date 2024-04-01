e66e491bd00441f37cc31e528cd87d1093a662ea
Solomon Berhe <sberhe@pacific.edu>
2024-04-01 15:42:25 -0700

    add select2 menu

--- src/arch/app.js
+++ src/arch/app.js
@@ -1,13 +1,23 @@
 import plantuml from '../../node_modules/@sakirtemel/plantuml.js/plantuml.js';
 
+// PRODUCTION
+/*
 const URL_API_ENDPOINT = "https://releasetrain.io/api";
+const urlSelectOptions = "https://releasetrain.io/api/c/names";
 const URL_HOMEPAGE = "https://releasetrain.io";
+*/
+
+// DEVELOPMENT
+const URL_API_ENDPOINT = "https://releasetrain.io/api";
+const urlSelectOptions = "https://releasetrain.io/api/c/names";
+const URL_HOMEPAGE = "http://localhost:8080/src";
 
 let versions = [];
 let tree = {};
+let counterId = 0;
 
 const urlParams = new URLSearchParams(window.location.search);
-const q = urlParams.get('q') || "";
+const q = urlParams.get('q') === null ? "" : urlParams.get('q');
 
 $.ajax({
     url: `${URL_API_ENDPOINT}/v?q=${encodeURIComponent(q)}`,
@@ -18,7 +28,7 @@ $.ajax({
         console.error("Error fetching data:", textStatus, errorThrown);
     }
 });
-    
+
 function handleData(data) {
     if (!Array.isArray(data)) {
         console.error("Data is not an array");
@@ -27,8 +37,9 @@ function handleData(data) {
     
     data.sort((a, b) => new Date(a.versionReleaseDate) - new Date(b.versionReleaseDate));
     versions.push(...data);
-    versionsToTree();
     
+    versionsToTree();
+
     plantuml.initialize('../app/node_modules/@sakirtemel/plantuml.js')
         .then(() => {
             console.log('PlantUML initialized');
@@ -38,6 +49,62 @@ function handleData(data) {
             console.error('Error initializing PlantUML:', error);
         });
 }
+
+$('#mySelect2').select2({
+    ajax: {
+        url: urlSelectOptions,
+        processResults: function(data) {
+            counterId = 1000;
+            let resultsArray = [];
+            data.forEach(version => {
+
+                if (version.hasOwnProperty('versionProductName') === false) {
+                    return;
+                }
+
+                if (isFirstLetterAlphabetic(version.versionProductName) === false) {
+                    return;
+                }
+
+                resultsArray.push({ "id": counterId, "text": version.versionProductName })
+
+                counterId = counterId + 1;
+            })
+
+            return {
+                results: resultsArray
+            };
+        }
+    }
+});
+
+function isFirstLetterAlphabetic(str) {
+    if (str.length === 0) return false; // Check if the string is empty
+    return /^[A-Za-z]/.test(str.charAt(0));
+}
+
+$('#mySelect2').on('change', function() {
+    var selectedTexts = $(this).find(':selected').map(function() {
+        return $(this).text();
+    }).get();
+    var valuesAsString = selectedTexts.join(',');
+    var urlToCall = URL_HOMEPAGE + '/arch?q=' + valuesAsString;
+    window.location.href = urlToCall;
+});
+   
+q.split(",").forEach(component => {
+
+    console.log(q, component);
+
+    if (q === "") {
+        return;
+    }
+    let initialValue = { id: counterId, text: component };
+    let newOption = new Option(initialValue.text, initialValue.id, true, true);
+    // $('#mySelect2').append(newOption).trigger('change');
+    $('#mySelect2').append(newOption);
+    counterId = counterId + 1;
+})
 
 function versionsToTree() {
     versions.forEach(v => {
