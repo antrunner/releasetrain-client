162bf0fb267d95f4e2a872c95eea2dafd6cc590e
sberhe <solomon.berhe@gmail.com>
2024-06-23 13:02:03 -0700

    fix grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,15 +3,14 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
-            build: {
-                src: 'src/app.js',
-                dest: 'dist/app.min.js'
-            }
-        },
+        // uglify: {
+        //     options: {
+        //         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        //     },
+        //     build: {
+
+        //     }
+        // },
         minifyHtml: {
             options: {
                 cdata: true
@@ -22,48 +21,42 @@ module.exports = function(grunt) {
                     'dist/app.css': 'src/app.css',
                     'dist/common/github.html': 'src/common/github.html',
                     'dist/common/navigation.html': 'src/common/navigation.html',
-                    'dist/conference/index.html': 'src/conference/index.html',
-                    'dist/developer/index.html': 'src/developer/index.html',
                     'dist/edi40/index.html': 'src/edi40/index.html',
                     'dist/edi40/2024/index.html': 'src/edi40/2024/index.html',
                     'dist/edi40-2023/index.html': 'src/edi40-2023/index.html',
                     'dist/graph/index.html': 'src/graph/index.html',
                     'dist/index.html': 'src/index.html',
                     'dist/doc/index.html': 'src/doc/index.html',
-                    'dist/api/index.html': 'src/api/index.html',
                     'dist/research/index.html': 'src/research/index.html',
                     'dist/reset.css': 'src/reset.css'
                 }
             }
         },
+        // Configuration for copy tasks
         copy: {
-            main: {
+            dist: {
                 files: [
-                    { expand: true, cwd: 'src', src: ['./*'], dest: 'dist', filter: 'isFile' },
-                    { expand: true, cwd: 'src/ack', src: ['./*'], dest: 'dist/ack', filter: 'isFile' },
-                    { expand: true, cwd: 'src/api', src: ['./*'], dest: 'dist/api', filter: 'isFile' },
-                    { expand: true, cwd: 'src/arch', src: ['./*'], dest: 'dist/arch', filter: 'isFile' },
-                    { expand: true, cwd: 'src/common', src: ['./*'], dest: 'dist/common', filter: 'isFile' },
-                    { expand: true, cwd: 'src/conference', src: ['./*'], dest: 'dist/conference', filter: 'isFile' },
-                    { expand: true, cwd: 'src/developer', src: ['./*'], dest: 'dist/developer', filter: 'isFile' },
-                    { expand: true, cwd: 'src/doc', src: ['./*'], dest: 'dist/doc', filter: 'isFile' },
-                    { expand: true, cwd: 'src/edi40', src: ['./*'], dest: 'dist/edi40', filter: 'isFile' },
-                    { expand: true, cwd: 'src/edi40-2023/data', src: ['./*'], dest: 'dist/edi40-2023/data', filter: 'isFile' },
-                    { expand: true, cwd: 'src/edi40/2024', src: ['./*'], dest: 'dist/edi40/2024', filter: 'isFile' },
-                    { expand: true, cwd: 'src/graph', src: ['./*'], dest: 'dist/graph', filter: 'isFile' },
-                    { expand: true, cwd: 'src/img', src: ['./*'], dest: 'dist/img', filter: 'isFile' },
-                    { expand: true, cwd: 'src/lib', src: ['./*'], dest: 'dist/lib', filter: 'isFile' },
-                    { expand: true, cwd: 'src/research', src: ['./*'], dest: 'dist/research', filter: 'isFile' },
-                ],
-            },
+                    { expand: true, cwd: 'src/ack/', src: ['index.html'], dest: 'dist/ack/' },
+                    { expand: true, cwd: 'src/', src: ['app.css'], dest: 'dist/' },
+                    { expand: true, cwd: 'src/common/', src: ['github.html', 'navigation.html'], dest: 'dist/common/' },
+                    { expand: true, cwd: 'src/edi40/', src: ['index.html'], dest: 'dist/edi40/' },
+                    { expand: true, cwd: 'src/edi40/2024/', src: ['index.html'], dest: 'dist/edi40/2024/' },
+                    { expand: true, cwd: 'src/edi40-2023/', src: ['index.html'], dest: 'dist/edi40-2023/' },
+                    { expand: true, cwd: 'src/graph/', src: ['index.html'], dest: 'dist/graph/' },
+                    { expand: true, cwd: 'src/', src: ['index.html'], dest: 'dist/' },
+                    { expand: true, cwd: 'src/doc/', src: ['index.html'], dest: 'dist/doc/' },
+                    { expand: true, cwd: 'src/research/', src: ['index.html'], dest: 'dist/research/' },
+                    { expand: true, cwd: 'src/', src: ['reset.css'], dest: 'dist/' }
+                ]
+            }
         }
     });
 
     // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-contrib-uglify-es');
+    // grunt.loadNpmTasks('grunt-contrib-uglify-es');
     grunt.loadNpmTasks('grunt-minify-html');
     grunt.loadNpmTasks('grunt-contrib-copy');
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify', 'minifyHtml', 'copy']);
+    grunt.registerTask('default', ['copy']);
 };
